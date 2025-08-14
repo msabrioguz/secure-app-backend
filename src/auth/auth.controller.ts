39c5cc51bb198efbcb2545dfabbe41a6add8686d
mustafa.oguz <m.sabri.oguz@gmail.com>
2025-08-14 15:49:26 +0300

    Yeniden düzenleme ile DTO kullanımı ve test dosyalarının silinmesi.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,24 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  register(
-    @Body('email') email: string,
-    @Body('password') password: string,
-    @Body('name') name: string,
-    @Body('surname') surname: string,
-  ) {
-    return this.authService.register(email, password, name, surname);
+  register(@Body() body: RegisterDto) {
+    return this.authService.register(
+      body.email,
+      body.password,
+      body.name,
+      body.surname,
+    );
   }
 
   @Post('login')
-  login(@Body('email') email: string, @Body('password') password: string) {
-    return this.authService.login(email, password);
+  login(@Body() body: LoginDto) {
+    return this.authService.login(body.email, body.password);
   }
 }
