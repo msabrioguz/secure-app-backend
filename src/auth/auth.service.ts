a58b162ffeb30397ff8e78c5cd86da7b8323978d
Mustafa Sabri Oğuz <m.sabri.oguz@gmail.com>
2025-08-31 15:13:39 +0300

    RefreshToken metodu yeniden düzenlendi.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -73,7 +73,7 @@ export class AuthService {
       const tokens = await this.generateTokens(user);
       await this.usersService.updateRefreshToken(user.id, tokens.refresh_token);
 
-      return new BaseResponse(200, true, ResponseMessages.SUCCESS, tokens);
+      return tokens;
     } catch {
       throw new UnauthorizedException('Refresh token doğrulanamadı');
     }
