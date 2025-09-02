23738166ec6f79a7cc9231724365dffbf7c73c95
mustafa.oguz <m.sabri.oguz@gmail.com>
2025-09-02 15:14:24 +0300

    Kullanıcının Profil Resmi Yüklemesi İşlemleri

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,10 +5,17 @@ import {
   Request,
   Patch,
   Body,
+  UseInterceptors,
+  Post,
+  Req,
+  UploadedFile,
 } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { UsersService } from './users.service';
 import { GetUser } from '_common/decorators/get-user.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 interface User {
   id: number;
@@ -48,5 +55,35 @@ export class UsersController {
     @GetUser('id') userId: number,
   ) {
     return this.usersService.updateProfile(userId, updateData);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('upload-avatar')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads/avatars',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          callback(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+        },
+      }),
+      fileFilter: (req, file, callback) => {
+        if (!file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+          return callback(new Error('Only image files are allowed!'), false);
+        }
+        callback(null, true);
+      },
+    }),
+  )
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  uploadProfilePic(@UploadedFile() file: Express.Multer.File, @Req() req) {
+    // Burada DB'de user tablosuna path kaydedebilirsiniz
+    return {
+      message: 'Profile picture uploaded successfully',
+      filePath: `/uploads/avatars/${file.filename}`,
+    };
   }
 }
